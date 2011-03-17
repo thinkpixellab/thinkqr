c02095a186a0c4f0f87a4b13f538d158f3dab54a
Kevin Moore <kevin@thinkpixellab.com>
2011-03-16 20:35:01 -0500

    Square: constants, defaults, more inertia

--- coffee/Square.coffee
+++ coffee/Square.coffee
@@ -4,19 +4,22 @@ goog.require 'goog.math.Vec2'
 goog.require 'goog.math.Coordinate'
 
 class Square
-  constructor: () ->
-    this.current = new goog.math.Coordinate()
+  constructor: (x = 0, y = 0) ->
+    this.current = new goog.math.Coordinate(x, y)
     this.target = new goog.math.Coordinate()
     this.velocity = new goog.math.Vec2(0, 0)
 
   update: () ->
     force = goog.math.Vec2.difference(this.target, this.current)
 
-    this.velocity.scale(0.8)
-    this.velocity.add(force)
-    if(this.velocity.magnitude() > 5)
-      this.velocity.normalize().scale(5)
+    this.velocity.scale(1 - Square.DRAG)
+    this.velocity.add(force.scale(0.01))
+    if(this.velocity.magnitude() > Square.TV)
+      this.velocity.normalize().scale(Square.TV)
 
     this.current.x += this.velocity.x
     this.current.y += this.velocity.y
     return
+
+Square.DRAG = 0.1
+Square.TV = 20
--- js/Square.js
+++ js/Square.js
@@ -3,21 +3,29 @@ goog.provide('Square');
 goog.require('goog.math.Vec2');
 goog.require('goog.math.Coordinate');
 Square = (function() {
-  function Square() {
-    this.current = new goog.math.Coordinate();
+  function Square(x, y) {
+    if (x == null) {
+      x = 0;
+    }
+    if (y == null) {
+      y = 0;
+    }
+    this.current = new goog.math.Coordinate(x, y);
     this.target = new goog.math.Coordinate();
     this.velocity = new goog.math.Vec2(0, 0);
   }
   Square.prototype.update = function() {
     var force;
     force = goog.math.Vec2.difference(this.target, this.current);
-    this.velocity.scale(0.8);
-    this.velocity.add(force);
-    if (this.velocity.magnitude() > 5) {
-      this.velocity.normalize().scale(5);
+    this.velocity.scale(1 - Square.DRAG);
+    this.velocity.add(force.scale(0.01));
+    if (this.velocity.magnitude() > Square.TV) {
+      this.velocity.normalize().scale(Square.TV);
     }
     this.current.x += this.velocity.x;
     this.current.y += this.velocity.y;
   };
   return Square;
 })();
+Square.DRAG = 0.1;
+Square.TV = 20;
