c4a5065c7ba50d44a36fa5f7dcda28d032ef4b48
Kevin Moore <kevin@thinkpixellab.com>
2011-04-09 16:56:51 -0700

    build...

---
+++ build_app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import glob
+from os import path
+from tools.Closure import Closure
+from tools.Shared import *
+
+js_path = "js"
+
+def main():
+  closure_path = path.join('deps', 'closure-library','closure')
+
+  js_dirs = ['js']
+  js_dirs += glob.glob('deps/easel/src/easeljs/**/')
+  deps_js_path = path.join(js_path, "deps.js")
+  compiled_js_path = path.join(js_path, "compiled.js")
+
+  externs = glob.glob(path.join(js_path, 'externs', '*.js'))
+
+  Closure(
+    closure_path = closure_path,
+    application_js_path = None,
+    root_symbol = 'Application',
+    closure_dependencies = js_dirs,
+    deps_js_path = deps_js_path,
+    compiled_js_path = compiled_js_path,
+    extern_files = externs
+  ).build_and_process('index_source.html', 'index_compiled.html', debug = False, skip_build = False)
+
+if __name__ == '__main__':
+  main()
